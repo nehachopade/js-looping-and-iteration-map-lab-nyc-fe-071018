91bed65cf506f5385df741e13fa6363d63172e7f
Neha Chopade <nehachopade22@gmail.com>
2018-09-01 16:46:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file.
+
+function lowerCaseDrivers(drivers){
+
+    return drivers.map((driver)=>driver.toLowerCase())
+} 
+
+function nameToAttributes(drivers){
+  
+  return drivers.map((driver)=>{
+    const driverFirst=(driver.split(" "))[0];
+    const driverLast = (driver.split(" "))[1];
+    
+    fetch('api.openweathermap.org/data/2.5/weather?q=' +value)
+    .then(response => response.json)
+    .then(responseJSON) =>console.log(responseJSON)
+    
+}
