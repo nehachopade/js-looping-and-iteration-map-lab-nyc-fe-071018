5cc9105bdc43556101ba6d953f1f8eb7b3d73ec6
Neha Chopade <nehachopade22@gmail.com>
2018-09-01 17:00:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ function nameToAttributes(drivers){
   return drivers.map((driver)=>{
     const driverFirst=(driver.split(" "))[0];
     const driverLast = (driver.split(" "))[1];
-    const  {firstName: driverFirst, lastName: driverLast;}
+    let arrayOfdrivers = 
+    {firstName: driverFirst, 
+    lastName: driverLast;}
+    return arrayOfdrivers;
   })
 }
