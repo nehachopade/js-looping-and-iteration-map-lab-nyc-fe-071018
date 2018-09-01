98ef046dc008cf662a1cae597c36fc148cddea49
Neha Chopade <nehachopade22@gmail.com>
2018-09-01 16:52:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ function nameToAttributes(drivers){
   return drivers.map((driver)=>{
     const driverFirst=(driver.split(" "))[0];
     const driverLast = (driver.split(" "))[1];
-    
-    fetch('api.openweathermap.org/data/2.5/weather?q=' +value)
-    .then(response => response.json)
-    .then(responseJSON) =>console.log(responseJSON)
-    
+    return driverFirst + " " + driverLast;
+  })
 }
